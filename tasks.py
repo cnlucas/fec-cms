210fac017fb63fd9bd72383adf5f79deb66e3a70
Laura Beaufort <lbeaufort@fec.gov>
2021-01-19 17:23:04 -0500

    Temporary deploy to stage

--- tasks.py
+++ tasks.py
@@ -73,10 +73,10 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    #('stage', lambda _, branch: branch.startswith('release')),
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
-    # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
+    ('stage', lambda _, branch: branch == 'hotfix-deploy-lb'),
 )
 
 
